3ffeb13f2c68564e54d8e88772c7a243eb151347
wxxiong6 <601115211@qq.com>
2019-11-06 09:52:46 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,4 @@
 <?php
-ini_set("memory_limit", '2048M');
-
 $rsa = new RsaUtil();
 $pub = file_get_contents(__DIR__.'/szkingdom_pub.cer');
 $pri = file_get_contents(__DIR__.'/szkingdom_pri.pfx');
@@ -8,63 +6,17 @@ $pri = file_get_contents(__DIR__.'/szkingdom_pri.pfx');
 var_dump($rsa->setPublicKey($pub));
 var_dump($rsa->setPkcs12($pri, "12345678"));
 
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
-// var_dump("getPublicKey", $rsa->getPublicKey());
-// var_dump("getPrivateKey", $rsa->getPrivateKey());
-// var_dump("getPkcs12", $rsa->getPkcs12());
+var_dump("getPublicKey", $rsa->getPublicKey());
+var_dump("getPrivateKey", $rsa->getPrivateKey());
 
-$s_str = "盅艺术asdafasfsa盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城f硕士劳动厅革城盅艺术asdafasfsa盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城盅艺术asdafasfsaf硕士劳动厅革城f硕士劳动厅革城";
 
-$encryptedTemp = $rsa->encrypt($s_str);
+$encryptedTemp = $rsa->encrypt("AAAAAA");
 var_dump("encrypted", $encryptedTemp);
 $decryptedTemp = $rsa->decrypt($encryptedTemp);
-var_dump("decrypted",$decryptedTemp);
-
-
+var_dump("decrypted", $decryptedTemp);
+$sign = $rsa->sign("abc", 2);
+var_dump("sign", $sign, 2);
+$verify = $rsa->verify("abc", $sign, 2);
+var_dump("verify", $verify);
 
 // var_dump("split", $rsa->split("adc", 1));
-var_dump("end");
